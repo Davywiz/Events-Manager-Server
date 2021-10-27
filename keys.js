6da2a3bb4efe37678bad36dea7b1812d8d3e2bd5
Davywiz <67049603+Davywiz@users.noreply.github.com>
2021-10-27 18:30:21 +0100

    Delete keys.js

--- keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGO_URI: "mongodb+srv://David:dolphin11@mycluster.etl4y.mongodb.net/events_manager",
-    PAYSTACK_API: "sk_test_664408c917dd348960c12e39d1dd40c4d29aca37",
-};
